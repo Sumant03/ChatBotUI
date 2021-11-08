6c860a8e8ba80f822ec9f58f24f3147565ef2ea4
Sumant <sumant.sharma.nonu@gmail.com>
2021-11-08 18:00:33 +0530

    appUI

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, escape, request ,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+
+    return render_template('index.html')
+
+@app.route('/consult')
+def consult():
+
+    return render_template('consultation.html')
+
+app.run(debug=True)
